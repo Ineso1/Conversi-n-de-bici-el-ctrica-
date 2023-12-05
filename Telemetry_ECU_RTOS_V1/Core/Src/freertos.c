bd59a527b6b91511ade81604708d8bb7a2e612bb
Ineso1 <inesgamos@gmail.com>
2023-12-05 02:43:43 -0600

    mas :)

--- Telemetry_ECU_RTOS_V1/Core/Src/freertos.c
+++ Telemetry_ECU_RTOS_V1/Core/Src/freertos.c
@@ -27,6 +27,7 @@
 /* USER CODE BEGIN Includes */
 #include "usart.h"
 #include "i2c.h"
+#include "arm_math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -36,7 +37,9 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define SAMPLE_TIME 0.01
+#define NUM_TAPS 21
+#define BLOCK_SIZE 32
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -55,6 +58,34 @@ int16_t 	accel_data[3];
 float		acc_x, acc_y, acc_z;
 int		acc_x_int, acc_y_int, acc_z_int;
 
+const float32_t firCoeffs[NUM_TAPS] = {
+    -0.0162988415008265,
+    -0.0709271864972703,
+    0.0225851291371347,
+    0.0371278894358235,
+    -0.0134217682863348,
+    -0.0589846533854996,
+    0.0145099553645701,
+    0.103203330310147,
+    -0.0148779327102912,
+    -0.317339878494921,
+    0.515006915991497,
+    -0.317339878494921,
+    -0.0148779327102912,
+    0.103203330310147,
+    0.0145099553645701,
+    -0.0589846533854996,
+    -0.0134217682863348,
+    0.0371278894358235,
+    0.0225851291371347,
+    -0.0709271864972703,
+    -0.0162988415008265
+};
+float32_t firState[NUM_TAPS + BLOCK_SIZE - 1];  // Filter state buffer
+arm_fir_instance_f32 FIR_Filter;
+float32_t inputBuffer[BLOCK_SIZE];
+float32_t outputBuffer[BLOCK_SIZE];
+
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
 osThreadId RecepcionCANHandle;
@@ -63,6 +94,9 @@ osThreadId UARTcomHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
+void IMU_acceleration(void);
+void IMU_dsp(void);
+void acceleration_data_conversion(float acc_x, float acc_y, float acc_z, char *message);
 
 /* USER CODE END FunctionPrototypes */
 
@@ -96,7 +130,8 @@ void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackTy
   */
 void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN Init */
-
+	// Initialize FIR filter
+	arm_fir_init_f32(&FIR_Filter, NUM_TAPS, (float32_t*)firCoeffs, firState, BLOCK_SIZE);
   /* USER CODE END Init */
 
   /* USER CODE BEGIN RTOS_MUTEX */
@@ -218,31 +253,22 @@ void StartRecepcionCAN(void const * argument)
 void StartIMUprocess(void const * argument)
 {
   /* USER CODE BEGIN StartIMUprocess */
+	static size_t bufferIndex = 0; // Keeps track of the current position in the buffer
   /* Infinite loop */
   for(;;)
   {
 
-	 GetAccelData(&hi2c1, (uint8_t*)imu_readings);
+	 IMU_acceleration();
+	 inputBuffer[bufferIndex] = acc_z; // Store the current sample in the buffer
+	 bufferIndex++; // Move to the next position in the buffer
 
-	 accel_data[0] = (((int16_t)((uint8_t *)(imu_readings))[1] << 8) | ((uint8_t *)(imu_readings))[0]);      // Turn the MSB and LSB into a signed 16-bit value
-	 accel_data[1] = (((int16_t)((uint8_t *)(imu_readings))[3] << 8) | ((uint8_t *)(imu_readings))[2]);
-	 accel_data[2] = (((int16_t)((uint8_t *)(imu_readings))[5] << 8) | ((uint8_t *)(imu_readings))[4]);
+	 if (bufferIndex >= BLOCK_SIZE) {
+		 bufferIndex = 0; // Reset buffer index
+		 IMU_dsp();
+		 acceleration_data_conversion(acc_x, acc_y, outputBuffer[BLOCK_SIZE], message); //Sends the data
+	 }
 
-
-	 acc_x = ((float)(accel_data[0]))/100.0f; //m/s2
-	 acc_y = ((float)(accel_data[1]))/100.0f;
-	 acc_z = ((float)(accel_data[2]))/100.0f;
-
-	 acc_x_int = (int)acc_x;
-	 acc_y_int = (int)acc_y;
-	 acc_z_int = (int)acc_z;
-
-
-	 sprintf(message, "X: %d Y: %d Z: %d\r\n", acc_x_int, acc_y_int, acc_z_int);
-	 HAL_UART_Transmit(&huart2, (uint8_t *)message, strlen(message), HAL_MAX_DELAY);
-
-
-	 osDelay(100);
+	 osDelay(2);
   }
   /* USER CODE END StartIMUprocess */
 }
@@ -272,6 +298,43 @@ void StartUARTcom(void const * argument)
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
+void IMU_acceleration() {
+    uint8_t imu_readings[IMU_NUMBER_OF_BYTES];
+    GetAccelData(&hi2c1, imu_readings);
+
+    int16_t accel_data[3];
+    accel_data[0] = ((int16_t)(imu_readings[1] << 8) | imu_readings[0]);
+    accel_data[1] = ((int16_t)(imu_readings[3] << 8) | imu_readings[2]);
+    accel_data[2] = ((int16_t)(imu_readings[5] << 8) | imu_readings[4]);
+
+    acc_x = ((float)accel_data[0]) / 100.0f;
+    acc_y = ((float)accel_data[1]) / 100.0f;
+    acc_z = ((float)accel_data[2]) / 100.0f;
+}
+
+void IMU_dsp(void) {
+	arm_fir_f32(&FIR_Filter, inputBuffer, outputBuffer, BLOCK_SIZE);
+}
+
+void acceleration_data_conversion(float acc_x, float acc_y, float acc_z, char *message) {
+    // Calculate G-forces, roll, pitch, and velocity
+    float g_force_x = acc_x / 9.81f;
+    float g_force_y = acc_y / 9.81f;
+    float g_force_z = acc_z / 9.81f;
+
+    float roll = atan2(acc_y, acc_z) * 180.0f / PI;
+    float pitch = atan2(-acc_x, sqrt(acc_y * acc_y + acc_z * acc_z)) * 180.0f / PI;
+
+    static float velocity_x = 0.0f, velocity_y = 0.0f, velocity_z = 0.0f;
+    velocity_x += acc_x * SAMPLE_TIME;
+    velocity_y += acc_y * SAMPLE_TIME;
+    velocity_z += acc_z * SAMPLE_TIME;
+
+    sprintf(message, "GX: %.2f GY: %.2f GZ: %.2f Roll: %.2f Pitch: %.2f VX: %.2f VY: %.2f VZ: %.2f\r\n",
+            g_force_x, g_force_y, g_force_z,
+            roll, pitch,
+            velocity_x, velocity_y, velocity_z);
+}
 
 /* USER CODE END Application */
 
--- Telemetry_ECU_RTOS_V1/Debug/Core/Src/freertos.d
+++ Telemetry_ECU_RTOS_V1/Debug/Core/Src/freertos.d
@@ -50,7 +50,8 @@ Core/Src/freertos.o: ../Core/Src/freertos.c \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
- ../Core/Inc/usart.h ../Core/Inc/i2c.h
+ ../Core/Inc/usart.h ../Core/Inc/i2c.h \
+ ../Middlewares/ST/ARM/DSP/Inc/arm_math.h
 ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
 ../Core/Inc/FreeRTOSConfig.h:
 ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
@@ -107,3 +108,4 @@ Core/Src/freertos.o: ../Core/Src/freertos.c \
 ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
 ../Core/Inc/usart.h:
 ../Core/Inc/i2c.h:
+../Middlewares/ST/ARM/DSP/Inc/arm_math.h:
